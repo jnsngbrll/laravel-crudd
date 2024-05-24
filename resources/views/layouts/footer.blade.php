6615eca3fb6edf33bfc28376c34eef53b3d7081c
Jansen Gabrillo <Gabrillojansen@gmail.com>
2024-05-24 16:42:33 +0800

    Nice admin successfully integrated

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Laravel CRUD</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      Modified by <a href="https://bootstrapmade.com/">Jansen Gabrillo</a>
+    </div>
+</footer>
